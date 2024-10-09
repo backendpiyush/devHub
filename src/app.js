d6c2933b86167558a3b1fb36a838ff49e394891b
backendpiyush <dev.piyushdubey@gmail.com>
2024-10-09 11:28:05 +0530

    created sCRUD API && created login , signup API with password hashing

--- src/app.js
+++ src/app.js
@@ -1,21 +1,112 @@
 const express = require("express");
 const connectDb = require("./config/database");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Aayushi",
-    lastName: "Dubey",
-    age: 22,
-    email: "aayushi@gmail.com",
-  };
+  const { firstName, lastName, age, email, password } = req.body;
 
-  const user = User(userObj);
+  // Hash Password
 
-  await user.save();
+  const hashPassword = await bcrypt.hash(password, 10);
 
-  res.send("user created successfully");
+  const user = User({
+    firstName,
+    lastName,
+    age,
+    email,
+    password: hashPassword,
+  });
+
+  try {
+    await user.save();
+
+    res.send("user created successfully");
+  } catch (error) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log(isPasswordValid);
+    if (isPasswordValid) {
+      res.send("Login successful");
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (error) {
+    res.status(400).send("something went wrong" + " " + error);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(404).send("user not found");
+    } else {
+      res.send("user deleted successfully");
+    }
+  } catch (error) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const data = req.body;
+    const userId = req.body.userId;
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, { data });
+    if (!user) {
+      res.status(404).send("user not found");
+    } else {
+      res.send("user updated successfully");
+    }
+  } catch (error) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send("users not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something Went Wrong");
+  }
 });
 
 connectDb()
--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,37 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 10,
+    },
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 10,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
